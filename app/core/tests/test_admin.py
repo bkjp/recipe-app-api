675ab06d033db899ae26d699556a5fa9a6dd59b6
bkjp <jbetfien@gmail.com>
2022-10-20 22:08:12 -0400

    configure urls of drf_spectacular

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -2,11 +2,10 @@
 Tests for the django admin modification
 """
 
-from multiprocessing.connection import Client
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from django.test import client
+from django.test import Client
 
 class AdminTests(TestCase):
     """Tests for django admin."""
@@ -33,3 +32,15 @@ class AdminTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
